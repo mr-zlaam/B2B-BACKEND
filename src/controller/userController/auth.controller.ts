9942c5e50cebba7ae6138c77e42b0269bd3cac4c
zlaam <zlaam.dev@gmail.com>
2025-04-10 20:52:40 +0500

    chore: Migrate fp to oop

--- src/controller/userController/auth.controller.ts
+++ src/controller/userController/auth.controller.ts
@@ -3,19 +3,17 @@ import { type IUSER } from "../../db/schemas/user.schema.js";
 import { httpResponse } from "../../util/globalUtil/apiResponse.util.js";
 import { asyncHandler } from "../../util/globalUtil/asyncHandler.util.js";
 import { manageUsers } from "../../util/appUtil/authUtil/userAuth.util.js";
-import { database, type DatabaseClient } from "../../db/db.js";
-const db: DatabaseClient = database.db;
-export default {
-  registerUser: asyncHandler(async (req, res) => {
-    const { checkExistingUser, handleNewUser, handleUnverifiedUser, handleVerifiedUser } = manageUsers(db);
+import type { DatabaseClient } from "../../db/db.js";
+
+export class AuthController {
+  private readonly _db: DatabaseClient;
+  constructor(db: DatabaseClient) {
+    this._db = db;
+  }
+  public registerUser = asyncHandler(async (req, res) => {
+    const { handleVerifiedUser, handleUnverifiedUser, handleNewUser, checkExistingUser } = manageUsers(this._db);
+
     const userBody = req.body as IUSER;
-    // TODO: VALIDATION NEEDED
-    /*
-     * 1. Check if user already exists ✔️
-     * 2. If user exists then check if he/she is verified️️ ️✔️
-     * 3. If user exists and is verified then return error otherwise send otp token to verify his/her account ✔️
-     * 4. If user does not exist then create new user
-     */
     const existingUser = await checkExistingUser(userBody);
     if (existingUser && existingUser.length > 0) {
       const user = existingUser[0];
@@ -28,5 +26,5 @@ export default {
     /* if user doesn't exist at all*/
     await handleNewUser(userBody, res);
     httpResponse(req, res, reshttp.okCode, reshttp.okMessage, { message: "Please check your email account for verification" });
-  })
-};
+  });
+}
--- src/router/userRouter/auth.Router.ts
+++ src/router/userRouter/auth.Router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import authController from "../../controller/userController/auth.controller.js";
+import { AuthController } from "../../controller/userController/auth.controller.js";
 import { validator } from "../../middleware/validation.middleware.js";
 import { authValidationSchema } from "../../validation/userValidation/auth.validation.js";
-
+import { database } from "../../db/db.js";
 export const authRouter: Router = Router();
+
+const authController = new AuthController(database.db);
 
 authRouter.route("/registerUser").post(validator(authValidationSchema), authController.registerUser);
